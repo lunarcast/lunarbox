7bda280feae2cd40499f2babcdd82dd3b5a4fdd9
benjmhart <benjmhart@gmail.com>
2019-03-16 00:29:25 -0400

    clear body tag for SPA

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import Main from './output/Main';
 
 function main() {
+  const body = document.querySelector('body');
+  Array.from(body.children).forEach(element => {
+    body.removeChild(element);
+  });
   Main.main();
 }
 
-if (module.hot) {
-  module.hot.accept(function() {
-    // console.log('running main again');
-    // main();
-  });
-}
+// if (module.hot) {
+//   module.hot.accept(function() {
+//     // console.log('running main again');
+//     // main();
+//   });
+// }
 
 console.log('starting');
 main();
