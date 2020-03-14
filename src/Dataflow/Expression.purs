8874b2ee142636028a7cb38e19b5c3b7a8ae46b0
Matei Adriel <rafaeladriel11@gmail.com>
2020-03-14 22:52:12 +0200

    feat: POC Expression codegen for debugging purpouses

--- src/Dataflow/Expression.purs
+++ src/Dataflow/Expression.purs
@@ -13,6 +13,10 @@ derive instance literalEq :: Eq Literal
 
 derive instance literalOrd :: Ord Literal
 
+instance showLiteral :: Show Literal where
+  show (LInt i) = show i
+  show (LBool b) = show b
+
 data NativeExpression a b
   = NativeExpression Type (a -> b)
 
@@ -30,6 +34,17 @@ data Expression
   | Native (forall a b. NativeExpression a b)
 
 derive instance expressinEq :: Eq Expression
+
+-- POC codegen for easy printing
+instance showExpression :: Show Expression where
+  show (Variable v) = show v
+  show (FunctionCall f i) = show f <> " " <> show i
+  show (Lambda arg body) = "\\" <> show arg <> " -> " <> show body
+  show (Literal lit) = show lit
+  show (Let name value body) = "let " <> show name <> " = " <> show value <> " in " <> show body
+  show (If expr then' else') = "if " <> show expr <> " then " <> show then' <> " else " <> show else'
+  show (FixPoint expr) = "<Recursive>( " <> show expr <> " )"
+  show (Native (NativeExpression t _)) = "<Native>( " <> show t <> " )"
 
 -- HELPERS
 nullExpr :: Expression
