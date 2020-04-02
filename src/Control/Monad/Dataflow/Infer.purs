4f73f3904a97dc8319b89d31c50a81d940b286c9
Matei Adriel <rafaeladriel11@gmail.com>
2020-04-02 15:29:17 +0300

    feat: typeMaps!!!

--- src/Control/Monad/Dataflow/Infer.purs
+++ src/Control/Monad/Dataflow/Infer.purs
@@ -1,30 +1,59 @@
 module Lunarbox.Control.Monad.Dataflow.Infer
   ( InferState(..)
+  , InferOutput(..)
   , InferEnv(..)
   , Infer(..)
   , _count
   , _location
   , _typeEnv
+  , _constraints
+  , _typeMap
   , runInfer
   , withLocation
+  , createConstraint
+  , rememberType
   ) where
 
 import Prelude
 import Control.Monad.Error.Class (class MonadThrow)
 import Control.Monad.Except (Except, runExcept)
-import Control.Monad.RWS (RWST, evalRWST, local)
+import Control.Monad.RWS (RWST, asks, evalRWST, local, tell)
 import Control.Monad.Reader (class MonadAsk, class MonadReader)
 import Control.Monad.State (class MonadState)
 import Control.Monad.Writer (class MonadTell, class MonadWriter)
 import Data.Either (Either)
-import Data.Lens (Lens', iso, set)
+import Data.Lens (Lens', iso, set, view)
 import Data.Lens.Record (prop)
+import Data.Map as Map
 import Data.Newtype (class Newtype, unwrap, wrap)
 import Data.Symbol (SProxy(..))
 import Data.Tuple (Tuple)
-import Lunarbox.Data.Dataflow.Constraint (ConstraintSet)
-import Lunarbox.Data.Dataflow.TypeError (TypeError)
+import Lunarbox.Data.Dataflow.Constraint (Constraint(..), ConstraintSet(..))
+import Lunarbox.Data.Dataflow.Type (Type)
 import Lunarbox.Data.Dataflow.TypeEnv (TypeEnv)
+import Lunarbox.Data.Dataflow.TypeError (TypeError)
+import Lunarbox.Data.Lens (newtypeIso)
+
+-- This is the output accumulated by the infer monad.
+-- It contains a set of constraints 
+-- and a map of location -> type paris
+newtype InferOutput l
+  = InferOutput
+  { constraints :: ConstraintSet l
+  , typeMap :: Map.Map l Type
+  }
+
+derive instance newtypeInferOutput :: Newtype (InferOutput l) _
+
+derive newtype instance semigroupInferOutput :: Ord l => Semigroup (InferOutput l)
+
+derive newtype instance monoidInferOutput :: Ord l => Monoid (InferOutput l)
+
+_constraints :: forall l. Lens' (InferOutput l) (ConstraintSet l)
+_constraints = newtypeIso <<< prop (SProxy :: _ "constraints")
+
+_typeMap :: forall l. Lens' (InferOutput l) (Map.Map l Type)
+_typeMap = newtypeIso <<< prop (SProxy :: _ "typeMap")
 
 newtype InferState
   = InferState
@@ -56,34 +85,63 @@ _typeEnv = iso unwrap wrap <<< prop (SProxy :: _ "typeEnv")
 _location :: forall l. Lens' (InferEnv l) l
 _location = iso unwrap wrap <<< prop (SProxy :: _ "location")
 
+-- The infer monad is the place where the type inference algorithm runs
 newtype Infer l a
-  = Infer (RWST (InferEnv l) (ConstraintSet l) InferState (Except (TypeError l)) a)
+  = Infer (RWST (InferEnv l) (InferOutput l) InferState (Except (TypeError l)) a)
 
-runInfer :: forall l a. InferEnv l -> Infer l a -> Either (TypeError l) (Tuple a (ConstraintSet l))
+-- This is a helper to transform an Infer monad into a single value
+runInfer :: forall l a. InferEnv l -> Infer l a -> Either (TypeError l) (Tuple a (InferOutput l))
 runInfer env (Infer m) = runExcept $ evalRWST m env mempty
 
 -- run a monad in a specific location
-withLocation :: forall a l. l -> Infer l a -> Infer l a
+withLocation :: forall a l. Ord l => l -> Infer l a -> Infer l a
 withLocation = local <<< set _location
+
+-- Helper to create a constraint at the current location given 2 types
+createConstraint :: forall l. Ord l => Type -> Type -> Infer l Unit
+createConstraint typeLeft typeRight = do
+  source <- asks $ view _location
+  tell
+    $ InferOutput
+        { constraints:
+            ConstraintSet $ pure
+              $ Constraint
+                  { source
+                  , typeRight
+                  , typeLeft
+                  }
+        , typeMap: mempty
+        }
+
+-- helper to mark a type in the typemap at the current location
+rememberType :: forall l. Ord l => Type -> Infer l Type
+rememberType type' = do
+  location <- asks $ view _location
+  tell
+    $ InferOutput
+        { constraints: mempty
+        , typeMap: Map.singleton location type'
+        }
+  pure type'
 
 derive newtype instance functorInfer :: Functor (Infer l)
 
-derive newtype instance applyInfer :: Apply (Infer l)
+derive newtype instance applyInfer :: Ord l => Apply (Infer l)
 
-derive newtype instance applicativeInfer :: Applicative (Infer l)
+derive newtype instance applicativeInfer :: Ord l => Applicative (Infer l)
 
-derive newtype instance bindInfer :: Bind (Infer l)
+derive newtype instance bindInfer :: Ord l => Bind (Infer l)
 
-derive newtype instance monadInfer :: Monad (Infer l)
+derive newtype instance monadInfer :: Ord l => Monad (Infer l)
 
-derive newtype instance monadAskInfer :: MonadAsk (InferEnv l) (Infer l)
+derive newtype instance monadAskInfer :: Ord l => MonadAsk (InferEnv l) (Infer l)
 
-derive newtype instance monadReaderInfer :: MonadReader (InferEnv l) (Infer l)
+derive newtype instance monadReaderInfer :: Ord l => MonadReader (InferEnv l) (Infer l)
 
-derive newtype instance monadTellInfer :: MonadTell (ConstraintSet l) (Infer l)
+derive newtype instance monadTellInfer :: Ord l => MonadTell (InferOutput l) (Infer l)
 
-derive newtype instance monadWriterInfer :: MonadWriter (ConstraintSet l) (Infer l)
+derive newtype instance monadWriterInfer :: Ord l => MonadWriter (InferOutput l) (Infer l)
 
-derive newtype instance monadStateInfer :: MonadState InferState (Infer l)
+derive newtype instance monadStateInfer :: Ord l => MonadState InferState (Infer l)
 
-derive newtype instance monadThrowInfer :: MonadThrow (TypeError l) (Infer l)
+derive newtype instance monadThrowInfer :: Ord l => MonadThrow (TypeError l) (Infer l)
--- src/Control/Monad/Dataflow/Infer/InferExpression.purs
+++ src/Control/Monad/Dataflow/Infer/InferExpression.purs
@@ -6,17 +6,15 @@ import Prelude
 import Control.Monad.Error.Class (throwError)
 import Control.Monad.Reader (asks, local)
 import Control.Monad.State (gets, modify_)
-import Control.Monad.Writer (tell)
 import Data.Array (zip)
 import Data.Lens (over, view)
 import Data.Map as Map
 import Data.Maybe (Maybe(..))
 import Data.Set as Set
 import Data.Traversable (traverse)
-import Lunarbox.Control.Monad.Dataflow.Infer (Infer, _count, _location, _typeEnv, withLocation)
+import Lunarbox.Control.Monad.Dataflow.Infer (Infer, _count, _location, _typeEnv, createConstraint, rememberType, withLocation)
 import Lunarbox.Data.Dataflow.Class.Substituable (Substitution(..), apply, ftv)
-import Lunarbox.Data.Dataflow.Constraint (Constraint(..), ConstraintSet(..))
-import Lunarbox.Data.Dataflow.Expression (Expression(..), Literal(..), NativeExpression(..), VarName)
+import Lunarbox.Data.Dataflow.Expression (Expression(..), Literal(..), NativeExpression(..), VarName, getLocation)
 import Lunarbox.Data.Dataflow.Scheme (Scheme(..))
 import Lunarbox.Data.Dataflow.Type (TVarName(..), Type(..), typeBool, typeNumber)
 import Lunarbox.Data.Dataflow.TypeEnv (TypeEnv(..))
@@ -25,20 +23,14 @@ import Lunarbox.Data.Dataflow.TypeError (TypeError(..))
 
 -- Create a fewsh type variable
 -- Uses the state from within the Infer monad to prevent duplicates
-fresh :: forall l. Infer l Type
+fresh :: forall l. Ord l => Infer l Type
 fresh = do
   inferState <- gets $ view _count
   modify_ $ over _count (_ + 1)
   pure $ TVarariable $ TVarName $ "t" <> show inferState
 
--- Takes 2 types and creates a constraint which says those 2 types should be equal
-createConstraint :: forall l. Type -> Type -> Infer l Unit
-createConstraint t1 t2 = do
-  source <- asks $ view _location
-  tell $ ConstraintSet [ Constraint { leftType: t1, rightType: t2, source } ]
-
 -- Create a scope for a variable to be in
-createClosure :: forall l a. VarName -> Scheme -> Infer l a -> Infer l a
+createClosure :: forall l a. Ord l => VarName -> Scheme -> Infer l a -> Infer l a
 createClosure name scheme =
   let
     scope (TypeEnv env) = cleaned <> TypeEnv.singleton name scheme
@@ -48,7 +40,7 @@ createClosure name scheme =
     local $ over _typeEnv scope
 
 -- The opposite of generalie. Takes a Forall type and creates a type out of it it
-instantiate :: forall l. Scheme -> Infer l Type
+instantiate :: forall l. Ord l => Scheme -> Infer l Type
 instantiate (Forall q t) = do
   q' <- traverse (const fresh) q
   let
@@ -56,7 +48,7 @@ instantiate (Forall q t) = do
   pure $ apply scheme t
 
 -- The opposite of instantiate. Takes a type, finds all the unresolved variables and packs them in a Forall instance. 
-generalize :: forall l. Type -> Infer l Scheme
+generalize :: forall l. Ord l => Type -> Infer l Scheme
 generalize t = do
   env <- asks $ view _typeEnv
   let
@@ -64,7 +56,7 @@ generalize t = do
   pure $ Forall qunatifiers t
 
 -- Lookup a TypeEnv and return the type. If the type doen't exist an error is thrown 
-lookupEnv :: forall l. VarName -> Infer l Type
+lookupEnv :: forall l. Ord l => VarName -> Infer l Type
 lookupEnv var = do
   location <- asks $ view _location
   (TypeEnv env) <- asks $ view _typeEnv
@@ -72,44 +64,40 @@ lookupEnv var = do
     Nothing -> throwError $ UnboundVariable var location
     Just s -> instantiate s
 
--- Takes an expression and returns it's type
-infer :: forall l. Expression l -> Infer l Type
-infer = case _ of
-  Variable location name ->
-    withLocation location do
-      lookupEnv name
-  Lambda location param body ->
-    withLocation location do
-      tv <- fresh
-      t <- createClosure param (Forall [] tv) $ infer body
-      pure $ tv `TArrow` t
-  FunctionCall location func input ->
-    withLocation location do
-      funcType <- infer func
-      inputType <- infer input
-      tv <- fresh
-      createConstraint funcType (inputType `TArrow` tv)
-      pure tv
-  Let location name value body ->
-    withLocation location do
-      t <- infer value
-      inner <- generalize t
-      createClosure name inner (infer body)
-  If location condition onTrue onFalse ->
-    withLocation location do
-      conditionType <- infer condition
-      trueType <- infer onTrue
-      falseType <- infer onFalse
-      createConstraint conditionType typeBool
-      createConstraint trueType falseType
-      pure trueType
-  FixPoint location expression ->
-    withLocation location do
-      t <- infer expression
-      tv <- fresh
-      createConstraint (tv `TArrow` tv) t
-      pure tv
-  -- those 3 branches do not need the location
-  Native _ (NativeExpression t _) -> pure t
-  Literal _ (LInt _) -> pure typeNumber
-  Literal _ (LBool _) -> pure typeBool
+-- Infers a type and marks it location on the typeMap
+infer :: forall l. Ord l => Expression l -> Infer l Type
+infer expression =
+  withLocation (getLocation expression) do
+    type' <- case expression of
+      Variable _ name -> do
+        lookupEnv name
+      Lambda _ param body -> do
+        tv <- fresh
+        t <- createClosure param (Forall [] tv) $ infer body
+        pure $ tv `TArrow` t
+      FunctionCall _ func input -> do
+        funcType <- infer func
+        inputType <- infer input
+        tv <- fresh
+        createConstraint funcType (inputType `TArrow` tv)
+        pure tv
+      Let _ name value body -> do
+        t <- infer value
+        inner <- generalize t
+        createClosure name inner (infer body)
+      If _ condition onTrue onFalse -> do
+        conditionType <- infer condition
+        trueType <- infer onTrue
+        falseType <- infer onFalse
+        createConstraint conditionType typeBool
+        createConstraint trueType falseType
+        pure trueType
+      FixPoint _ body -> do
+        t <- infer body
+        tv <- fresh
+        createConstraint (tv `TArrow` tv) t
+        pure tv
+      Native _ (NativeExpression t _) -> pure t
+      Literal _ (LInt _) -> pure typeNumber
+      Literal _ (LBool _) -> pure typeBool
+    rememberType type'
