085a33ad9b6eca85690f28fb21a40629840ad383
Matei Adriel <rafaeladriel11@gmail.com>
2020-04-28 20:55:47 +0300

    feat: logic gates

--- src/Data/Dataflow/Native/Logic.purs
+++ src/Data/Dataflow/Native/Logic.purs
@@ -1,25 +1,80 @@
-module Lunarbox.Data.Dataflow.Native.Logic (evalNot, not') where
+module Lunarbox.Data.Dataflow.Native.Logic
+  ( evalNot
+  , not'
+  , or
+  , and
+  , xor
+  ) where
 
 import Prelude
 import Data.Maybe (Maybe(..))
 import Lunarbox.Data.Dataflow.Expression (NativeExpression(..))
 import Lunarbox.Data.Dataflow.Native.NativeConfig (NativeConfig(..))
-import Lunarbox.Data.Dataflow.Runtime (RuntimeValue(..))
+import Lunarbox.Data.Dataflow.Runtime (RuntimeValue(..), binaryFunction)
 import Lunarbox.Data.Dataflow.Scheme (Scheme(..))
-import Lunarbox.Data.Dataflow.Type (typeBool)
+import Lunarbox.Data.Dataflow.Type (Type(..), typeBool)
 import Lunarbox.Data.Editor.FunctionData (internal)
 import Lunarbox.Data.Editor.FunctionName (FunctionName(..))
 
+-- Evaluate a not function
 evalNot :: RuntimeValue -> RuntimeValue
 evalNot (Bool inner) = Bool $ not inner
 
 evalNot _ = Null
 
+-- Arguments for 2-inputs logic gates
+binaryLogicFunctionArgs :: Array ({ name :: String })
+binaryLogicFunctionArgs = [ { name: "first input" }, { name: "second input" } ]
+
+-- Wrap a logic binary function into a binary function operating on Runtime values
+binaryLogicFunction' :: (Boolean -> Boolean -> Boolean) -> RuntimeValue -> RuntimeValue -> RuntimeValue
+binaryLogicFunction' function (Bool a) (Bool b) = Bool $ function a b
+
+binaryLogicFunction' _ _ _ = Null
+
+-- Wrap a logic binary function into a Runtime value
+binaryLogicFunction :: (Boolean -> Boolean -> Boolean) -> RuntimeValue
+binaryLogicFunction = binaryFunction <<< binaryLogicFunction'
+
+-- The type for binary logic functions
+binaryLogicFunctionType :: Type
+binaryLogicFunctionType = TArrow typeBool $ TArrow typeBool typeBool
+
 not' :: forall h a. NativeConfig h a
 not' =
   NativeConfig
     { name: FunctionName "name"
-    , expression: NativeExpression (Forall [] typeBool) $ Function evalNot
+    , expression: NativeExpression (Forall [] $ TArrow typeBool typeBool) $ Function evalNot
     , functionData: internal [ { name: "input" } ]
+    , component: Nothing
+    }
+
+and :: forall h a. NativeConfig h a
+and =
+  NativeConfig
+    { name: FunctionName "add"
+    , expression: NativeExpression (Forall [] binaryLogicFunctionType) $ binaryLogicFunction (&&)
+    , functionData: internal binaryLogicFunctionArgs
+    , component: Nothing
+    }
+
+or :: forall h a. NativeConfig h a
+or =
+  NativeConfig
+    { name: FunctionName "or"
+    , expression: NativeExpression (Forall [] binaryLogicFunctionType) $ binaryLogicFunction (||)
+    , functionData: internal binaryLogicFunctionArgs
+    , component: Nothing
+    }
+
+evalXor :: RuntimeValue
+evalXor = binaryLogicFunction \a b -> a || b && (not $ a && b)
+
+xor :: forall h a. NativeConfig h a
+xor =
+  NativeConfig
+    { name: FunctionName "xor"
+    , expression: NativeExpression (Forall [] binaryLogicFunctionType) evalXor
+    , functionData: internal binaryLogicFunctionArgs
     , component: Nothing
     }
--- src/Data/Dataflow/Native/Prelude.purs
+++ src/Data/Dataflow/Native/Prelude.purs
@@ -6,14 +6,27 @@ module Lunarbox.Data.Dataflow.Native.Prelude
 import Lunarbox.Data.Dataflow.Native.ControlFlow (if')
 import Lunarbox.Data.Dataflow.Native.Function (const', identity', pipe)
 import Lunarbox.Data.Dataflow.Native.Literal (boolean, false', true')
-import Lunarbox.Data.Dataflow.Native.Logic (not')
+import Lunarbox.Data.Dataflow.Native.Logic (and, not', or, xor)
 import Lunarbox.Data.Dataflow.Native.Math (add)
 import Lunarbox.Data.Dataflow.Native.NativeConfig (NativeConfig, loadNativeConfigs)
 import Lunarbox.Data.Editor.State (State)
 
 -- Array with all the built in nodes
 configs :: forall h a. Array (NativeConfig h a)
-configs = [ add, if', pipe, identity', const', true', false', boolean, not' ]
+configs =
+  [ add
+  , if'
+  , pipe
+  , identity'
+  , const'
+  , true'
+  , false'
+  , boolean
+  , not'
+  , and
+  , or
+  , xor
+  ]
 
 -- Load all the built in nodes
 loadPrelude :: forall h a. State h a -> State h a
