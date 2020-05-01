23f10a3d8fb70b3d9c6e5bd5efe16030876b6bc8
Matei Adriel <rafaeladriel11@gmail.com>
2020-05-01 13:12:43 +0300

    feat: number helpers

--- src/Data/Dataflow/Native/Prelude.purs
+++ src/Data/Dataflow/Native/Prelude.purs
@@ -7,34 +7,35 @@ import Lunarbox.Data.Dataflow.Native.ControlFlow (if')
 import Lunarbox.Data.Dataflow.Native.Function (const', identity', pipe)
 import Lunarbox.Data.Dataflow.Native.Literal (boolean, false', number, string, true')
 import Lunarbox.Data.Dataflow.Native.Logic (and, not', or, xor)
-import Lunarbox.Data.Dataflow.Native.Math (add)
+import Lunarbox.Data.Dataflow.Native.Math (mathNodes)
 import Lunarbox.Data.Dataflow.Native.NativeConfig (NativeConfig, loadNativeConfigs)
 import Lunarbox.Data.Dataflow.Native.String (concatStrings, reverseString, stringLength, trimString)
 import Lunarbox.Data.Editor.State (State)
+import Prelude
 
 -- Array wita s mll the built in nodes
 configs :: forall a s m. Array (NativeConfig a s m)
 configs =
-  [ add
-  , if'
-  , pipe
-  , identity'
-  , const'
-  , true'
-  , false'
-  , boolean
-  , number
-  , string
-  , not'
-  , and
-  , or
-  , xor
-  -- String helpers 
-  , reverseString
-  , concatStrings
-  , stringLength
-  , trimString
-  ]
+  mathNodes
+    <> [ if'
+      , pipe
+      , identity'
+      , const'
+      , true'
+      , false'
+      , boolean
+      , number
+      , string
+      , not'
+      , and
+      , or
+      , xor
+      -- String helpers 
+      , reverseString
+      , concatStrings
+      , stringLength
+      , trimString
+      ]
 
 -- Load all the built in nodes
 loadPrelude :: forall a s m. State a s m -> State a s m
